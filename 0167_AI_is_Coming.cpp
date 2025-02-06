f11d972ec2ee3a9549367e7e106d7829d1803b70
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-06 19:46:45 +0530

    Create 0167_AI_is_Coming.cpp

---
+++ 0167_AI_is_Coming.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    
+    int x;
+    cin>>x;
+    
+    if(x >= 0 &&  x <= 60 ){
+        cout<<"YES"<<endl;
+    }
+    else if (x > 60 && x <= 100){
+        cout<<"NO"<<endl;
+    }
+}
