51a4a102f0bd350ac09ce07ff0d1492f1b50dbdf
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-06 20:02:31 +0530

    Create 0177_Entry_Check.cpp

---
+++ 0177_Entry_Check.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin>>x;
+    
+    if(x >= 10){
+        cout<<"YES"<<endl;
+    }
+    else if(x>=0 && x < 10){
+        cout<<"NO"<<endl;
+    }
+}
