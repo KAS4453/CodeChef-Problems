d36a49eee394f96aa9253ae8c24f4a23619ee7bb
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-07 19:36:42 +0530

    Create 0212_Chef_and_Socks.cpp

---
+++ 0212_Chef_and_Socks.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int a, x, y, sum;
+    cin>>a>>x>>y;
+    
+    sum = x+y;
+    
+    if(a <= sum){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    
+}
