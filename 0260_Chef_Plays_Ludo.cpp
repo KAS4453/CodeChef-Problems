ad56a147033a805c9b29785335b368fff4c63128
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-09 16:02:30 +0530

    Create 0260_Chef_Plays_Ludo.cpp

---
+++ 0260_Chef_Plays_Ludo.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    int x;
+    
+    for(int i = 0 ; i < t ; i++){
+        cin>>x;
+        if(x==6){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
