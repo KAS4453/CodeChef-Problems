3dc384c70f00b033de1d0c1ecac328d92c270f72
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-10 14:08:42 +0530

    Create 0271_International_Education_Day!.cpp

---
+++ 0271_International_Education_Day!.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int a, b, c;
+    cin>>a>>b>>c;
+    
+    if(a>=b){
+        cout<<a*c<<endl;
+    }
+    else{
+        cout<<b*c<<endl;
+    }
+}
