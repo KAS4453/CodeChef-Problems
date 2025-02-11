7e6995c8ca78b178f256611874f852f594719614
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-11 10:50:53 +0530

    Create 0295_Parity.cpp

---
+++ 0295_Parity.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t, n;
+    cin>>t;
+    
+    while(t--){
+        cin>>n;
+        if(n%2 == 0){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
+}
