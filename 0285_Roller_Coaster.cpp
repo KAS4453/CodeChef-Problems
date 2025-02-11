c1781a3db0da513dfb92c605def9aecb50986cc4
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-11 10:34:58 +0530

    Create 0285_Roller_Coaster.cpp

---
+++ 0285_Roller_Coaster.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t, x, h;
+    cin>>t;
+    
+    while(t--){
+        cin>>x>>h;
+        if(x>=h){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
