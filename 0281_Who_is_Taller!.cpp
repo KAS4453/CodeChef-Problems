be52a3db9ff8c62d73eeee3f4a8249eb6943c9f3
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-11 10:21:24 +0530

    Create 0281_Who_is_Taller!.cpp

---
+++ 0281_Who_is_Taller!.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t, x, y;
+    cin>>t;
+    
+    while(t--){
+        cin>>x>>y;
+        if(x>y){
+            cout<<"A"<<endl;
+        }
+        else{
+            cout<<"B"<<endl;
+        }
+    }
+}
