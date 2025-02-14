38a0a7c2115cab85c0052ac6d41e1458ead7ada8
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-14 13:06:57 +0530

    Create 0299_Right_There.cpp

---
+++ 0299_Right_There.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t, n, x;
+    cin>>t;
+    
+    while(t--){
+        cin>>n>>x;
+        if(n<=x){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
