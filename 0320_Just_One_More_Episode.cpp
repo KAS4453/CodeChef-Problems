597334a8eef156df3f243cf28998f6b0fcb13ed6
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-14 23:14:13 +0530

    Create 0320_Just_One_More_Episode.cpp

---
+++ 0320_Just_One_More_Episode.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t, x;
+    cin>>t;
+
+    while(t--){
+        cin>>x;
+        if(x>=24){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
+}
