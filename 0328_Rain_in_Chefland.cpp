7ab34126fdc86c87043b3b88cacaaa2c6a7999e2
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-15 11:52:54 +0530

    Create 0328_Rain_in_Chefland.cpp

---
+++ 0328_Rain_in_Chefland.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t, x;
+    cin>>t;
+    
+    while(t--){
+        cin>>x;
+        if(x<3){
+            cout<<"LIGHT"<<endl;
+        }
+        else if(x>=7){
+            cout<<"HEAVY"<<endl;
+        }
+        else{
+            cout<<"MODERATE"<<endl;
+        }
+    }
+}
