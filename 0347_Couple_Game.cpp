7c96bed74725ef621e50e846fa9760c22baa4287
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-16 14:17:08 +0530

    Create 0347_Couple_Game.cpp

---
+++ 0347_Couple_Game.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t, g, b;
+    cin>>t;
+    
+    while(t--){
+        cin>>g>>b;
+        cout<<b-g<<endl;
+    }
+}
