f1e9302263e04935dd34dcf74d4c170126a2a183
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-16 15:06:34 +0530

    Create 0355_Access_Code_Equality.cpp

---
+++ 0355_Access_Code_Equality.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    string s;
+    cin>>s;
+    if(s == "WECNITK"){
+        cout<<"Welcome to Web Club!"<<endl;
+    }
+    else{
+        cout<<"Access denied"<<endl;
+    }
+}
