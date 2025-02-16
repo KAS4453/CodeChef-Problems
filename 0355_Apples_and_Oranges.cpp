53f9d9137690bde3e282eb22981a6f8f9a56dcf5
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-16 15:03:44 +0530

    Create 0355_Apples_and_Oranges.cpp

---
+++ 0355_Apples_and_Oranges.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x, a, b;
+    cin>>x;
+    cin>>a>>b;
+    if(x>=a+b){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
