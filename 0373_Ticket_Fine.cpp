37d1f9b9ffe860320ab202f6527adb14a9e2179d
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-17 16:43:48 +0530

    Create 0373_Ticket_Fine.cpp

---
+++ 0373_Ticket_Fine.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t, x, p, q;
+    cin>>t;
+    
+    while(t--){
+        cin>>x>>p>>q;
+        cout<<x*(p-q)<<endl;
+    }
+}
