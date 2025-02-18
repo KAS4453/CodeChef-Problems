088005ea62a2f00aba697f372582964e255e9b87
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-18 23:21:52 +0530

    Create 0405_Chef_and_Chocolates.cpp

---
+++ 0405_Chef_and_Chocolates.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y,z;
+        cin>>x>>y>>z;
+        // your code goes here
+        cout<<(5*x+10*y)/z<<endl;
+    }
+}
