85496d4f25c9dae0aee6f6a257c805e1f3c41387
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-18 13:54:43 +0530

    Create 0374_Oneful_Pairs.cpp

---
+++ 0374_Oneful_Pairs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+	// your code goes here
+	
+	if((a*b + a + b)  == 111){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
