e8dbdceb608971fa264790660029b49e4962940e
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-19 00:02:02 +0530

    Create 0419_Fill_the_Bucket.cpp

---
+++ 0419_Fill_the_Bucket.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t, x, k;
+    cin>>t;
+    while(t--){
+        cin>>k>>x;
+        cout<<k-x<<endl;
+    }
+}
