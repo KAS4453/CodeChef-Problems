340491053a43ad6781dc8d051b2a66f669815646
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-18 22:20:03 +0530

    Create 0382_Worth_of_a_Video.cpp

---
+++ 0382_Worth_of_a_Video.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t, s;
+    cin>>t;
+    
+    while(t--){
+        cin>>s;
+        cout<<24*1000*s<<endl;
+    }
+}
