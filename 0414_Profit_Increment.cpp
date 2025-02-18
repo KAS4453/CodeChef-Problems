20166cfca7983756c2daa52f3e59791ed0154a9a
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-18 23:58:12 +0530

    Create 0414_Profit_Increment.cpp

---
+++ 0414_Profit_Increment.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t, x, y;
+    cin>>t;
+    
+    while(t--){
+        cin>>x>>y;
+        cout<<(0.1)*x + y <<endl;
+    }
+}
