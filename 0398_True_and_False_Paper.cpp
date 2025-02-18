53bbf80a745e7d4ac0986541f3e6a38e14faa186
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-18 23:12:55 +0530

    Create 0398_True_and_False_Paper.cpp

---
+++ 0398_True_and_False_Paper.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t, n, k;
+    cin>>t;
+    
+    while(t--){
+        cin>>n>>k;
+        cout<<n-k<<endl;
+    }
+}
