e71846b0f00ca5be4f019feccae8f8c8b66b9976
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-18 22:21:52 +0530

    Create 0383_Chef_and_Wire_Frames.cpp

---
+++ 0383_Chef_and_Wire_Frames.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t, n, m, x;
+    cin>>t;
+    
+    while(t--){
+        cin>>n>>m>>x;
+        cout<<2*(n+m)*x<<endl;
+    }
+}
