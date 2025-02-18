e0cbde23865b33e7ea138f41d43f7683f5d10cd9
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-18 23:48:45 +0530

    Create 0410_Is_It_Hot_or_Cold.cpp

---
+++ 0410_Is_It_Hot_or_Cold.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t, c;
+    cin>>t;
+    
+    while(t--){
+        cin>>c;
+        if(c>20){
+            cout<<"HOT"<<endl;
+        }
+        else{
+            cout<<"COLD"<<endl;
+        }
+    }
+}
