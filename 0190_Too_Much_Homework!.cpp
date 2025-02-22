406a296a17b0672f7acbe0d3c26bcff050b0d2fa
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-02-22 22:16:33 +0530

    Create 0190_Too_Much_Homework!.cpp

---
+++ 0190_Too_Much_Homework!.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x, y;
+    cin>>x>>y;
+    
+    if(x+10*y >= 100){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+}
